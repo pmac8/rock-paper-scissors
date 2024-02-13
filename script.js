d7c991b6d70b7fb340b4c17d9b85b4278848c8c4
pmac8 <29241044+pmac8@users.noreply.github.com>
2024-02-13 15:19:48 -0500

    Add function of a single round of the game

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-let choice = ['rock','paper','scissors'];
+// Get computer to randomly select choices
+
+const choice = ['rock','paper','scissors'];
 function getComputerChoice(choice){
     return choice[Math.floor(Math.random()*choice.length)];
 }
 
+// play a single round of the game
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice(choice);
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+    playerSelection === 'paper' && computerSelection === 'rock' ||
+    playerSelection === 'scissors' && computerSelection === 'paper'){
+        return 'You Win!'
+    } else if (playerSelection === computerSelection){
+        return 'Tie!'
+    } else {
+        return 'You Lose!'
+    }
+} 
+console.log(playRound(playerSelection, computerSelection))
