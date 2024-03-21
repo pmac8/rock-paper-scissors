576f340c3ed56de5563f213e8357795550e03d74
pmac8 <29241044+pmac8@users.noreply.github.com>
2024-03-21 12:28:42 -0400

    Add buttons to game with correct selection being displayed

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ function getComputerChoice(choice){
 
 // play a single round of the game
 
-const playerSelection = 'rock';
+const playerSelection = 0;
 const computerSelection = getComputerChoice(choice);
 function playRound(playerSelection, computerSelection){
+    
+    
     if (playerSelection === 'rock' && computerSelection === 'scissors' ||
     playerSelection === 'paper' && computerSelection === 'rock' ||
     playerSelection === 'scissors' && computerSelection === 'paper'){
@@ -21,13 +23,41 @@ function playRound(playerSelection, computerSelection){
     }
 } 
 
+
 // play multiple rounds of the game
 
-function playGame(){
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('What do you pick?');
-        let computerSelection = getComputerChoice(choice);
-        console.log(`Round ${i}: ${playRound(playerSelection, computerSelection)}`);
-      }
-}
-playGame();
+//function playGame(){
+   // for (let i = 0; i < 5; i++) {
+      //  let playerSelection = prompt('What do you pick?');
+      //  let computerSelection = getComputerChoice(choice);
+      //  console.log(`Round ${i}: ${playRound(playerSelection, computerSelection)}`);
+    //  }
+//}
+//playGame();
+
+const button = document.createElement("button");
+button.innerText = "Rock";
+button.id = "rock";
+button.addEventListener("click", playRound => {
+    console.log(playerSelection);
+})
+document.body.appendChild(button);
+
+const buttonTwo = document.createElement("button");
+buttonTwo.innerText = "Paper";
+buttonTwo.id = "paper";
+buttonTwo.addEventListener("click", playRound  => {
+    console.log("paper");
+})
+document.body.appendChild(buttonTwo);
+
+const buttonThree = document.createElement("button");
+buttonThree.innerText = "Scissor";
+buttonThree.id = "scissor";
+buttonThree.addEventListener("click", playRound  => {
+    console.log("scissor");
+})
+document.body.appendChild(buttonThree);
+
+const div = document.createElement("div");
+
