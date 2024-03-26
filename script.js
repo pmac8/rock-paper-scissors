a153658f8e0cef76750b0ce5909295f326a98614
pmac8 <29241044+pmac8@users.noreply.github.com>
2024-03-26 15:06:22 -0400

    Add buttons and score card to html and add play functions to javascript

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+const outcomeDiv = document.querySelector(".outcome");
+const playerScoreSpan = document.querySelector(".player-score");
+const computerScoreSpan = document.querySelector(".computer-score");
+
 // Get computer to randomly select choices
 
 const choice = ['rock','paper','scissors'];
@@ -7,23 +14,75 @@ function getComputerChoice(choice){
 
 // play a single round of the game
 
-const playerSelection = 0;
+let playerSelection = 0;
+let playerScore = 0;
+let computerScore = 0;
+
 const computerSelection = getComputerChoice(choice);
 function playRound(playerSelection, computerSelection){
-    
-    
     if (playerSelection === 'rock' && computerSelection === 'scissors' ||
     playerSelection === 'paper' && computerSelection === 'rock' ||
     playerSelection === 'scissors' && computerSelection === 'paper'){
-        return 'You Win!'
+        playerScore++
+        const p = document.createElement('p')
+        p.innerText = 'You win!'
+        outcomeDiv.appendChild(p)
     } else if (playerSelection === computerSelection){
-        return 'Tie!'
+        const p = document.createElement('p')
+        p.innerText = 'Tie!'
+        outcomeDiv.appendChild(p)
     } else {
-        return 'You Lose!'
+        computerScore++
+        const p = document.createElement('p')
+        p.innerText = 'You Lose!'
+        outcomeDiv.appendChild(p)
     }
 } 
 
+const checkForWinner = (playerScore, computerScore) => {
+    if (playerScore === 5){
+        const h2 = document.createElement('h2')
+        h2.classList.add('player-win')
+        h2.innerText = 'You won great job!'
+        outcomeDiv.append(h2)
+    } 
+    
+    if (computerScore === 5) {
+        const h2 = document.createElement('h2')
+        h2.classList.add('computer-win')
+        h2.innerText = 'You lost try again!'
+        outcomeDiv.append(h2)
+    }
+}
 
+const updateScores = (playerScore, computerScore) => {
+    playerScoreSpan.innerText = `Players Score: ${playerScore}`
+    computerScoreSpan.innerText = `Computers Score: ${computerScore}`
+}
+
+rockButton.addEventListener('click', () => {
+    const computerSelection = getComputerChoice(choice);
+    const playerSelection = 'rock';
+    playRound(playerSelection, computerSelection);
+    updateScores(playerScore,computerScore)
+    checkForWinner(playerScore, computerScore)
+})
+
+paperButton.addEventListener('click', () => {
+    const computerSelection = getComputerChoice(choice);
+    const playerSelection = 'paper';
+    playRound(playerSelection, computerSelection);
+    updateScores(playerScore,computerScore)
+    checkForWinner(playerScore, computerScore)
+})
+
+scissorsButton.addEventListener('click', () => {
+    const computerSelection = getComputerChoice(choice);
+    const playerSelection = 'scissors';
+    playRound(playerSelection, computerSelection);
+    updateScores(playerScore,computerScore)
+    checkForWinner(playerScore, computerScore)
+})
 // play multiple rounds of the game
 
 //function playGame(){
@@ -34,30 +93,3 @@ function playRound(playerSelection, computerSelection){
     //  }
 //}
 //playGame();
-
-const button = document.createElement("button");
-button.innerText = "Rock";
-button.id = "rock";
-button.addEventListener("click", playRound => {
-    console.log(playerSelection);
-})
-document.body.appendChild(button);
-
-const buttonTwo = document.createElement("button");
-buttonTwo.innerText = "Paper";
-buttonTwo.id = "paper";
-buttonTwo.addEventListener("click", playRound  => {
-    console.log("paper");
-})
-document.body.appendChild(buttonTwo);
-
-const buttonThree = document.createElement("button");
-buttonThree.innerText = "Scissor";
-buttonThree.id = "scissor";
-buttonThree.addEventListener("click", playRound  => {
-    console.log("scissor");
-})
-document.body.appendChild(buttonThree);
-
-const div = document.createElement("div");
-
